9a214ac3c0451564b9c480d0fc6ade11ec091a64
Snailvvvip <snailvvvip@163.com>
2026-05-23 10:42:18 +0800

    feat：删除

--- projects/base-admin-server/app/server.py
+++
@@ -1,5 +0,0 @@
-from app.run_uvicorn import run_uvicorn
-
-if __name__ == "__main__":
-  run_uvicorn()
-
--- projects/base-admin-server/app/utils/path_join.py
+++
@@ -1,6 +0,0 @@
-import os
-from typing import List
-
-
-def path_join(*pat_list: List[str]):
-  return os.path.join(*pat_list).replace('\\', '/')
